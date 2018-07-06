c3ed8d25682bddd2812d07bc2f2b6441466b26fd
admin <you@example.com>
2018-07-06 10:14:54 +0200

    My Djs app, first commit

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,  get_object_or_404
+from django.utils import timezone
+from .models import Post
+
+# Create your views here.
+
+def post_list(request, pk):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': Post})
+
+def post_detail(request, pk):
+    Post.objects.get(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+
+
+
+
+
+
